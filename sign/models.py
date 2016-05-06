7092f23112365f9f488219210cb53b96b133c99e
Denis Roldan <denis.roldan.estebanez@gmail.com>
2016-05-06 13:17:10 +0200

    Added verbose options to the model and str function

--- sign/models.py
+++ sign/models.py
@@ -9,3 +9,10 @@ class Sign(models.Model):
     text = models.CharField(max_length=200)
     timestamp = models.DateTimeField(auto_now_add=True)
     expire_datetime = models.DateTimeField()
+
+    def __str__(self):
+        return "{0} created by {1} on {2}".format(self.text, self.author, self.timestamp)
+
+    class Meta:
+        verbose_name = "Soul Sign"
+        verbose_name_plural = "Soul Signs"
