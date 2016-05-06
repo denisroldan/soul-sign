a1dd3e9e2acaf5b09dc1ebc961c7a1ba509e4d96
Denis Roldan <denis.roldan.estebanez@gmail.com>
2016-05-06 12:23:54 +0200

    Added Sign Model and initial schema

--- sign/models.py
+++ sign/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.six import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Sign(models.Model):
+    author = models.ForeignKey(User)
+    text = models.CharField(max_length=200)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    expire_datetime = models.DateTimeField()
