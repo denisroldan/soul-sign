6b35f32447e97376c13bdab9bb86641491fcb977
Denis Roldan <denis.roldan.estebanez@gmail.com>
2016-05-08 16:34:34 +0200

    Modified Sign model, created migration file

--- sign/models.py
+++ sign/models.py
@@ -7,11 +7,11 @@ from django.utils.six import python_2_unicode_compatible
 class Sign(models.Model):
     author = models.ForeignKey(User)
     text = models.CharField(max_length=200)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    expire_datetime = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    expires = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return "{0} created by {1} on {2}".format(self.text, self.author, self.timestamp)
+        return "{0} created by {1} on {2}".format(self.text, self.author, self.created)
 
     class Meta:
         verbose_name = "Soul Sign"
